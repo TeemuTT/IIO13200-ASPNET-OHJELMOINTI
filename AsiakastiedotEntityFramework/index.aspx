4f59e07c88eb039c216d2908c29fb141826f9b26
TeemuTT <tuomela.teemu@gmail.com>
2016-11-21 13:15:16 +0200

    Tehtävä 5 valmis

---
+++ AsiakastiedotEntityFramework/index.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="AsiakastiedotEntityFramework.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+    <%-- Header --%>
+    <div>
+        <asp:Button runat="server" Text="Lataa kaikki asiakkaat" OnClick="Unnamed1_Click" />
+        <asp:DropDownList runat="server" ID="ddMaat"/>
+        <asp:Button runat="server" Text="Hae asiakkaat valitusta maasta" OnClick="Unnamed2_Click" />
+        <asp:Button runat="server" Text="Hae asiakkaat maittain" OnClick="Unnamed3_Click" /> 
+    </div>
+
+    <%-- Content --%>
+    <div>
+        <asp:GridView runat="server" ID="gridView" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ AsiakastiedotEntityFramework/index.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+namespace AsiakastiedotEntityFramework
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                using (var db = new DemoxOyEntities())
+                {
+                    var query = from t in db.asiakas
+                                select t.maa;
+
+                    // Valitaan uniikit maat DropDownListiin.
+                    ddMaat.DataSource = query.Distinct().ToList();
+                    ddMaat.DataBind();
+                }
+            }
+        }
+
+        private void loadAllCustomers()
+        {
+            using (var db = new DemoxOyEntities())
+            {
+                var query = from a in db.asiakas
+                            select a;
+
+                gridView.DataSource = query.ToList();
+                gridView.DataBind();
+            }
+        }
+
+        private void loadFromCountry(String country)
+        {
+            using (var db = new DemoxOyEntities())
+            {
+                var query = from a in db.asiakas
+                            where a.maa == country
+                            select a;
+
+                gridView.DataSource = query.ToList();
+                gridView.DataBind();
+            }
+        }
+
+        private void groupByCountry(string country)
+        {
+            using (var db = new DemoxOyEntities())
+            {
+                var query = from a in db.asiakas
+                            orderby a.maa
+                            select a;
+
+                gridView.DataSource = query.ToList();
+                gridView.DataBind();
+            }
+        }
+
+        // Kaikki.
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            loadAllCustomers();
+        }
+
+        // Valitusta maasta.
+        protected void Unnamed2_Click(object sender, EventArgs e)
+        {
+            loadFromCountry(ddMaat.SelectedValue);
+        }
+
+        // Lajiteltuna maittain.
+        protected void Unnamed3_Click(object sender, EventArgs e)
+        {
+            groupByCountry(ddMaat.SelectedValue);
+        }
+    }
+}
